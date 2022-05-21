1b40d47516df557bc596adce3e3eca01ccca9b71
NITINSHELAR999 <80057048+NITINSHELAR999@users.noreply.github.com>
2022-05-21 08:49:32 +0530

    Update Login.jsx

--- src/Loginpage/Login.jsx
+++ src/Loginpage/Login.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-import { useState } from 'react'
+import {useEffect, useState } from 'react'
 
 function Login() {
-    
+  const [password, setPassword] = useState("")
+  const [color, setColor] = useState("black")
+  const [strength, setStrength] = useState("")
+  //regular expression
+  var regex = new Array();
+  regex.push("[A-Z]"); //Uppercase Alphabet.
+  regex.push("[a-z]"); //Lowercase Alphabet.
+  regex.push("[0-9]"); //Digit.
+  regex.push("[$@$!%*#?&]");//spestial character
+var pass=0
+//validation for eachregular expression
+regex.forEach((ele) => {
+
+  if (new RegExp(ele).test(password)) {
+
+    pass++
+  }
+
+});
+  //validation for length of the password
+  if (pass > 2 && password.length > 8) {
+    pass++
+}
+// displayed status condtion check kartoy 
+const Validation=()=>{
+    if(pass==0 || pass==1|| pass==2){
+        setStrength("low");
+        setColor("red");
+    }
+    if(pass==4 || pass==3 ){
+        setStrength("medium");
+        setColor("yello");
+    }
+    if(pass==5 || pass==6){
+        setStrength("high");
+        setColor("green");
+    }
+  }
+  useEffect(()=>{
+    Validation()
+},[password])
   return (
    
     <div className="bg">
@@ -16,7 +56,8 @@ function Login() {
           <input type="text" id="fname" name="firstname" placeholder="Enter Your email"/>
       
           <label for="lname">Password</label>
-          <input type="text" id="lname" name="lastname" placeholder="Enter your password"/>
+          <input type="text" onChange={(e) => setPassword(e.target.value)} onKeyUp={(e)=>{setPassword(e.target.value)}} id="lname" name="lastname" placeholder="Enter your password"/>
+          <h3 ><span style={{color:color}}>{strength}</span></h3>
           <div className="page">
           
 
@@ -35,5 +76,7 @@ function Login() {
 }
 
 export default Login
+
+
 
 
